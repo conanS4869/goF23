89ec848d92e2d23d65f19453531c3affeb16f9bb
conanS4869 <87404816@qq.com>
2020-03-06 09:01:46 +0800

    pool

---
+++ src/com/conan/proxy/pool/TestPool.java
@@ -0,0 +1,24 @@
+package com.conan.proxy.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestPool {
+    public static void main(String[] args) {
+        ExecutorService service= Executors.newFixedThreadPool(10);
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+
+        service.shutdown();
+    }
+}
+
+class MyThread implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
